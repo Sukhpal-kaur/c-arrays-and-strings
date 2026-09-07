10a8b135f5ff5f60117c2b86a9f86fd8260a698f
Sukhpal kaur <sukhpalkaur063@gmail.com>
2026-09-07 15:43:15 +0530

    Create call_by_reference.c

---
+++ call_by_reference.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void changeValue(int *x)
+{
+    *x = 100;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Before function call = %d\n", num);
+
+    changeValue(&num);
+
+    printf("After function call = %d", num);
+
+    return 0;
+}
