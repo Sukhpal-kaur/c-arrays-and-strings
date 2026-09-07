d4317ac1de8a195bd8a92b0956b090ca3775dfae
Sukhpal kaur <sukhpalkaur063@gmail.com>
2026-09-07 15:20:54 +0530

    Create count_vowels.c

---
+++ count_vowels.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, vowels = 0;
+
+    printf("Enter a string: ");
+    scanf("%99[^\n]", str);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == 'a' || str[i] == 'e' ||
+            str[i] == 'i' || str[i] == 'o' ||
+            str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' ||
+            str[i] == 'I' || str[i] == 'O' ||
+            str[i] == 'U')
+        {
+            vowels++;
+        }
+
+        i++;
+    }
+
+    printf("Number of vowels = %d", vowels);
+
+    return 0;
+}
